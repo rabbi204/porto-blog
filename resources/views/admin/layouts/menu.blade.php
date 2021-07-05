4c93c8479216e54387ce4f600e72d3bdae937637
rabbi204 <md.rabbi.web@gmail.com>
2021-07-05 10:44:11 +0600

    Teamplate slice done

---
+++ resources/views/admin/layouts/menu.blade.php
@@ -0,0 +1,35 @@
+<div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+            <ul>
+                <li class="menu-title">
+                    <span>Main</span>
+                </li>
+                <li class="active">
+                    <a href="{{ route('admin.dashboard') }}"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+                </li>
+                
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-document"></i> <span> Blogs</span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="invoice-report.html">Add New Post</a></li>
+                        <li><a href="invoice-report.html">All Post</a></li>
+                        <li><a href="invoice-report.html">Category</a></li>
+                        <li><a href="invoice-report.html">Tag</a></li>
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-document"></i> <span> Product</span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="invoice-report.html">All Product</a></li>
+                        <li><a href="invoice-report.html">Category</a></li>
+                        <li><a href="invoice-report.html">Tag</a></li>
+                        <li><a href="invoice-report.html">Brand</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</div>
